26a126e93448c0f4ce23842fc8d802a36a50c624
Dom <domdotdudley@gmail.com>
2023-03-29 15:31:08 +0100

    add author, time and comment info

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   useEffect(() => {
     dispatch(loadPosts())
-  },[dispatch])
+   },[dispatch])
 
 
   return (
--- src/features/post/post.js
+++ src/features/post/post.js
@@ -1,13 +1,23 @@
 import React from "react";
 import styles from './post.module.css'
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect, useState } from "react";
-import { loadPosts,  selectPosts} from "./postSlice";
+import { useSelector } from "react-redux";
+import { selectPosts} from "./postSlice";
 
 export function Post() {
 
     const posts = useSelector(selectPosts)
     console.log(posts)
+
+    const timeHelper = (timeA, timeB) => {
+        const mins = ((timeB - timeA)) / 60
+        const hours  = mins / 60;
+        if(hours > 1){
+            return `${Math.floor(hours)} hours ago`
+        } else {
+            return `${Math.floor(mins)} minutes ago`
+        }
+    }
+
 
     return(
         <div>
@@ -16,8 +26,13 @@ export function Post() {
                 <div className={styles.postItem}>
                 <p className={styles.subredditName}>{post.data.subreddit_name_prefixed}</p>
                 <h3 className={styles.postTitle}>{post.data.title}</h3>
-                <img className={styles.postImage} src={post.data.url} alt='post'/>
-                <button>Comments</button>
+                {post.data.is_reddit_media_domain ? 
+                <img className={styles.postImage} src={post.data.url} alt='post'/> : ''}
+                <div className={styles.postInfo}>
+                    <p>Posted By <span className={styles.postAuthor}>{post.data.author}</span></p>
+                    <p>{timeHelper(post.data.created_utc, Math.floor(new Date().getTime()/1000) )}</p>
+                    <p>{post.data.num_comments} comments</p>
+                </div>
             </div>
             ))
         }
